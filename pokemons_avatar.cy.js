573045bca23f5bbd4666645b0ea9cd3cbd421d89
denbaskulin <133255742+denbaskulin@users.noreply.github.com>
2024-03-29 21:16:24 +0300

    Update pokemons_avatar.cy.js

--- pokemons_avatar.cy.js
+++ pokemons_avatar.cy.js
@@ -1,8 +1,8 @@
 describe('Покупка аватара', function () {                               // название набора тестов
     it('e2e тест на покупку нового аватара для тренера', function () {   // название теста
          cy.visit('https://pokemonbattle.me/');                          // переходим на сайт https://pokemonbattle.me/
-         cy.get('input[type="email"]').type('denbaskulin@yandex.ru');      // вводим логин
-         cy.get('input[type="password"]').type('Baskaden4554/');    // вводим пароль
+         cy.get('input[type="email"]').type('USER_LOGIN');      // вводим логин
+         cy.get('input[type="password"]').type('USER_PASSWORD');    // вводим пароль
          cy.get('button[type="submit"]').click();                        // нажимаем кнопку Подтвердить
          cy.get('.header__btns > [href="/shop"]').click();               // нажимаем кнопку Магазин
          cy.get('.available > button').first().click();                  // кликаем по кнопке Купить у первого доступного аватара
